6aa9689ea28e0780c05b1c6fd27a6a7f607abf79
smoncada19 <santiago.mp08@hotmail.com>
2024-06-05 14:48:55 -0500

    Se creo el menu de opciones

---
+++ menu_cajero.py
@@ -0,0 +1,9 @@
+def menu():
+    while True:
+        opcion = int(input("Menu...\n1. Consultar saldo\n2. Retirar saldo\n3. Consignar\n4. Cambiar clave\n5. Salir\n"))
+
+        if opcion == 5:
+            break
+
+
+menu()
