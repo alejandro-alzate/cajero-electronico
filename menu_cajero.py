17b6866a9bdbd4edc822a4b5f757031416bd7db0
sebastian <juan2016.bg@gmail.com>
2024-06-05 15:09:27 -0500

    menu principal para logearse

--- menu_cajero.py
+++ menu_cajero.py
@@ -1,3 +1,10 @@
+def menu_inicio():
+    while True:
+        opcion = int(input("Menu_inicio...\n1.reqistrar \n2.ingresar \n3. salir"))
+
+        if opcion == 3:
+            break
+
 def menu():
     while True:
         opcion = int(input("Menu...\n1. Consultar saldo\n2. Retirar saldo\n3. Consignar\n4. Cambiar clave\n5. Salir\n"))
