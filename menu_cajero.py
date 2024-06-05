e461690babd0aa0ee15a580a145a4aab2e7a632e
miguelg420 <miguelgrisales13@gmail.com>
2024-06-05 15:28:31 -0500

    añadido consulta, variables:

--- menu_cajero.py
+++ menu_cajero.py
@@ -1,8 +1,17 @@
+import os
+saldo=3000000
+
 def menu():
     while True:
         opcion = int(input("Menu...\n1. Consultar saldo\n2. Retirar saldo\n3. Consignar\n4. Cambiar clave\n5. Salir\n"))
-
+        os.system('cls')
+        
+        if opcion == 1:
+            print(f"Su saldo actual es: ${saldo}")
+            
+        
         if opcion == 5:
+            print("Cerrando Sesion...")
             break
 
 
